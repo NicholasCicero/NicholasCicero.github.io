df49506fa72c6556ac2d6c51e19cf4f0b6c1139d
NicholasCicero <nicholas.cicero381@tctchome.com>
2024-12-20 07:01:48 -0800

    Merge remote-tracking branch 'origin/main'

--- js/nav.js
+++ js/nav.js
@@ -6,11 +6,11 @@ const createNav = () => {
     <img src="/img/stagefright_logo_web.png" class="brand-logo" alt="LOGO" height="100" width="350">
 
     <nav class="navbar" style="font-family: 'Poppins', sans-serif">
-        <a href="index.html" class="metal-mania-regular">Home</a>
-        <a href="store.html" class="metal-mania-regular">Store</a>
-        <a href="stagefright_website/tourdates.html" class="metal-mania-regular">Tour Dates</a>
-        <a href="stagefright_website/about.html" class="metal-mania-regular">About</a>
-        <a href="stagefright_website/contact.html" class="metal-mania-regular">Contact</a>
+        <a href="../stagefright/index.html" class="metal-mania-regular">Home</a>
+        <a href="../stagefright/store.html" class="metal-mania-regular">Store</a>
+        <a href="../stagefright/tourdates.html" class="metal-mania-regular">Tour Dates</a>
+        <a href="../stagefright/about.html" class="metal-mania-regular">About</a>
+        <a href="../stagefright/contact.html" class="metal-mania-regular">Contact</a>
     </nav>
 </header>
     `;
